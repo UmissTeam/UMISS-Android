9360fe54f76704f8d517a224bfb1352bf8e251e5
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2017-04-24 21:08:45 -0300

    Adding login activity hard coded

---
+++ app/src/main/java/com/umiss/LoginActivity.java
@@ -0,0 +1,81 @@
+package com.umiss;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.JsonToken;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) throws JSONException {
+
+        EditText user = (EditText) findViewById(R.id.editText_user);
+        EditText password = ((EditText) findViewById(R.id.editText_password));
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("data",MODE_PRIVATE);
+        JSONObject jsonObject = authenticate(user.getText().toString(), password.getText().toString(), token);
+
+        if ( jsonObject != null ){
+
+            pushLoginCredentials(sharedPreferences, jsonObject);
+            startMainActivity();
+        }else{
+
+            loginHasFailed(user, password);
+        }
+    }
+
+    private JSONObject authenticate(String user, String password, String token) throws JSONException {
+
+        JSONObject jsonObject = new JSONObject();
+
+        //TODO: Connect with server
+        if ( user.equals("hard") && password.equals("coded") ){
+
+            jsonObject.put("authenticationToken", "authenticationToken");
+            return jsonObject;
+        }
+
+        return null;
+    }
+
+    private void pushLoginCredentials(SharedPreferences sharedPreferences, JSONObject jsonObject) throws JSONException {
+
+        SharedPreferences.Editor prefEditor = sharedPreferences.edit();
+        prefEditor.putBoolean("isLogged", true);
+        //TODO: in case this token is necessary
+        prefEditor.putString("authenticationToken", jsonObject.getString("authenticationToken"));
+        prefEditor.commit();
+    }
+
+    private void loginHasFailed(EditText user, EditText password){
+
+        Toast.makeText(this, "Invalid credentials!", Toast.LENGTH_SHORT).show();
+        user.setText("");
+        password.setText("");
+        user.requestFocus();
+    }
+
+    private void startMainActivity(){
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
